7034db3b17b13acd8a483ee79a590d82db2e36bf
github username <github email address>
2019-06-25 17:49:26 -0400

    all features working

--- src/index.js
+++ src/index.js
@@ -8,16 +8,36 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector('#create-task-form')
   form.addEventListener('submit', function(e) {
     e.preventDefault()
-    console.log(e.target)
 
     const taskDescription = document.querySelector("#new-task-description").value
-    const p = document.createElement('p')
-    mainContent.innerText = taskDescription
-    form.reset()
+    const dueDate = document.querySelector("#due-date").value
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    const prioritySelection = document.querySelector("#priority-selection").value
+    let x = 0
+    if (prioritySelection === "high-priority"){
+      x = taskDescription.fontcolor("red");
+    }
+    if (prioritySelection === "medium-priority"){
+      x = taskDescription.fontcolor("#FFE05E");
+    }
+    if (prioritySelection === "low-priority"){
+      x = taskDescription.fontcolor("green");
+    }
+    deleteButton.innerText = "X"
+    li.innerHTML = x + ": " + dueDate
+    li.appendChild(deleteButton);
+    deleteButton.addEventListener('click', function(e){
+      li.remove();
+    })
+
+    document.getElementById('tasks').appendChild(li)
+
+    console.log(taskDescription)
+    form.reset();
     console.log(e.target)
 
-    // document.getElementById("tasks").add
    });
-   // <ul id="tasks">
-   //  </ul>
+
+
 });
